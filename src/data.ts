7f35510cec60ba43d04ff9da45f2495c65beade4
aaa1-error <64493728+aaa1-error@users.noreply.github.com>
2024-05-12 21:49:25 +0300

    replaced jpegs with webps

--- src/data.ts
+++ src/data.ts
@@ -1,41 +1,41 @@
 export const cold_faces = [
-  "/hljpegs/cold_faces/20240506134058_1.jpg",
-  "/hljpegs/cold_faces/20240506134122_1.jpg",
-  "/hljpegs/cold_faces/20240506134135_1.jpg",
-  "/hljpegs/cold_faces/20240506134148_1.jpg"
+  "/hljpegs/cold_faces/20240506134058_1.webp",
+  "/hljpegs/cold_faces/20240506134122_1.webp",
+  "/hljpegs/cold_faces/20240506134135_1.webp",
+  "/hljpegs/cold_faces/20240506134148_1.webp"
 ]
 
 export const crossfire = [
-  "/hljpegs/crossfire/20240506133810_1.jpg",
-  "/hljpegs/crossfire/20240506133834_1.jpg",
-  "/hljpegs/crossfire/20240506133901_1.jpg",
-  "/hljpegs/crossfire/20240506133925_1.jpg",
-  "/hljpegs/crossfire/20240506133935_1.jpg",
-  "/hljpegs/crossfire/20240506134001_1.jpg"
+  "/hljpegs/crossfire/20240506133810_1.webp",
+  "/hljpegs/crossfire/20240506133834_1.webp",
+  "/hljpegs/crossfire/20240506133901_1.webp",
+  "/hljpegs/crossfire/20240506133925_1.webp",
+  "/hljpegs/crossfire/20240506133935_1.webp",
+  "/hljpegs/crossfire/20240506134001_1.webp"
 ]
 
 export const doublecross = [
-  "/hljpegs/doublecross/20240506130830_1.jpg",
-  "/hljpegs/doublecross/20240506130838_1.jpg",
-  "/hljpegs/doublecross/20240506130853_1.jpg",
-  "/hljpegs/doublecross/20240506130936_1.jpg",
-  "/hljpegs/doublecross/20240506130953_1.jpg",
-  "/hljpegs/doublecross/20240506131239_1.jpg",
+  "/hljpegs/doublecross/20240506130830_1.webp",
+  "/hljpegs/doublecross/20240506130838_1.webp",
+  "/hljpegs/doublecross/20240506130853_1.webp",
+  "/hljpegs/doublecross/20240506130936_1.webp",
+  "/hljpegs/doublecross/20240506130953_1.webp",
+  "/hljpegs/doublecross/20240506131239_1.webp",
 ]
 
 export const rapidcore = [
-  "/hljpegs/rapidcore/20240506131409_1.jpg",
-  "/hljpegs/rapidcore/20240506131411_1.jpg",
-  "/hljpegs/rapidcore/20240506131426_1.jpg",
-  "/hljpegs/rapidcore/20240506131440_1.jpg",
-  "/hljpegs/rapidcore/20240506131449_1.jpg",
-  "/hljpegs/rapidcore/20240506131529_1.jpg"
+  "/hljpegs/rapidcore/20240506131409_1.webp",
+  "/hljpegs/rapidcore/20240506131411_1.webp",
+  "/hljpegs/rapidcore/20240506131426_1.webp",
+  "/hljpegs/rapidcore/20240506131440_1.webp",
+  "/hljpegs/rapidcore/20240506131449_1.webp",
+  "/hljpegs/rapidcore/20240506131529_1.webp"
 ]
 
 export const stalkx = [
-  "/hljpegs/stalkx/20240506130227_1.jpg",
-  "/hljpegs/stalkx/20240506130252_1.jpg",
-  "/hljpegs/stalkx/20240506130312_1.jpg",
-  "/hljpegs/stalkx/20240506130329_1.jpg",
-  "/hljpegs/stalkx/20240506130610_1.jpg"
+  "/hljpegs/stalkx/20240506130227_1.webp",
+  "/hljpegs/stalkx/20240506130252_1.webp",
+  "/hljpegs/stalkx/20240506130312_1.webp",
+  "/hljpegs/stalkx/20240506130329_1.webp",
+  "/hljpegs/stalkx/20240506130610_1.webp"
 ]
