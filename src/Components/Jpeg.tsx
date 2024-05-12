d67d294f2cc88bdb43f6c37fc08222302321218a
aaa1-error <64493728+aaa1-error@users.noreply.github.com>
2024-05-12 22:19:57 +0300

    i'm crying

--- src/Components/Jpeg.tsx
+++ src/Components/Jpeg.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Jpeg = (props : Props) => {
   return (
-    <animated.img loading={"lazy"} className="origin-top px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 max-h-[90vh] object-cover" src={props.image} alt={props.image}/>
+    <animated.img className="origin-top px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 max-h-[90vh] object-cover" src={props.image} alt={props.image}/>
   )
 }
 
